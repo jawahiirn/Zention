fea642eb6282e7cbe656430ddc680eb2f8f22ca0
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-07-03 23:35:50 +0600

    re-init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,41 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+export default tseslint.config(
+  { ignores: ["dist"] }, // Ignore build output folder
+  {
+    extends: [
+      js.configs.recommended, // Recommended JavaScript rules
+      ...tseslint.configs.recommended, // Recommended TypeScript rules
+      prettier, // Disable conflicting ESLint rules
+    ],
+    files: ["**/*.{ts,tsx}"], // Apply to TypeScript files
+    languageOptions: {
+      ecmaVersion: 2020, // ECMAScript 2020 syntax
+      globals: globals.browser, // Enable browser global variables
+    },
+    plugins: {
+      "react-hooks": reactHooks, // React hooks rules
+      "react-refresh": reactRefresh, // React Fast Refresh rules
+      prettier: prettierPlugin, // Integrate Prettier into ESLint
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules, // Apply recommended React Hooks rules
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }, // React Fast Refresh rule
+      ],
+      "prettier/prettier": "warn", // Enforce Prettier formatting
+    },
+  },
+  {
+    files: ["**/shadcn-ui/*.tsx"],
+    rules: {
+      "react-refresh/only-export-components": "off",
+    },
+  },
+);
