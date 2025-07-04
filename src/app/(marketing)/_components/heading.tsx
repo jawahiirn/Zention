96ab17872fd73c74d341c149dfc8b90f5088fc6b
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-07-04 19:11:21 +0600

    Auth state added to Header

--- src/app/(marketing)/_components/heading.tsx
+++ src/app/(marketing)/_components/heading.tsx
@@ -1,9 +1,14 @@
 'use client';
 
+import { Spinner } from '@/components/spinner';
 import { Button } from '@/components/ui/button';
+import { SignInButton } from '@clerk/clerk-react';
+import { useConvexAuth } from 'convex/react';
 import { ArrowRightIcon } from 'lucide-react';
+import Link from 'next/link';
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div className={'max-w-3xl space-y-4'}>
       <h1 className={'text-3xl sm:text-5xl md:text-6xl font-bold'}>
@@ -14,10 +19,27 @@ export const Heading = () => {
         Zention is the connected workspace where <br />
         better, faster work happens.
       </h3>
-      <Button>
-        Enter Zention
-        <ArrowRightIcon className={'ml-2 size-4'} />
-      </Button>
+      {isLoading && (
+        <div className="flex w-full items-center justify-center">
+          <Spinner size="md" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Zention
+            <ArrowRightIcon className="ml-2 h-4 w-4" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Zention free
+            <ArrowRightIcon className="ml-2 h-4 w-4" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
