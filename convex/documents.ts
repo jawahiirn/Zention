b9e5d59e12d941f2220b8861ec290dd629771256
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-07-05 17:52:30 +0600

    Create Document API integration and functionality added.

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,6 +1,6 @@
 import { v } from 'convex/values';
 import { mutation, query } from './_generated/server';
-import { Doc, Id } from './_generated/dataModel';
+// import { Doc, Id } from './_generated/dataModel';
 
 export const get = query({
   args: {
