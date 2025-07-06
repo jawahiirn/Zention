5fd28240b4f4f42230e7709db9f5192c46f72bf0
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-07-06 23:25:12 +0600

    Add page button added to sidebar. Created API for trash items GET, Restore items and Delete document.

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,6 +1,6 @@
 import { v } from 'convex/values';
 import { mutation, query } from './_generated/server';
-import { Id } from './_generated/dataModel';
+import { Doc, Id } from './_generated/dataModel';
 
 export const getSidebar = query({
   args: {
@@ -91,5 +91,99 @@ export const archive = mutation({
     recursiveArchive(args.id);
 
     return document;
+  },
+});
+export const getTrash = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
+    const userId = identity.subject;
+
+    return await ctx.db
+      .query('documents')
+      .withIndex('by_user', (q) => q.eq('userId', userId))
+      .filter((q) => q.eq(q.field('isArchived'), true))
+      .order('desc')
+      .collect();
+  },
+});
+export const restore = mutation({
+  args: { id: v.id('documents') },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
+    const userId = identity.subject;
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error('Document not found');
+    }
+    if (existingDocument.userId !== userId) {
+      throw new Error('User is unauthorized');
+    }
+
+    const recursiveRestore = async (docId: Id<'documents'>) => {
+      const children = await ctx.db
+        .query('documents')
+        .withIndex('by_user_parent', (q) =>
+          q.eq('userId', userId).eq('parentDocument', docId)
+        )
+        .collect();
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: false,
+        });
+
+        await recursiveRestore(child._id);
+      }
+    };
+
+    const options: Partial<Doc<'documents'>> = {
+      isArchived: false,
+    };
+    if (existingDocument.parentDocument) {
+      const parent = await ctx.db.get(existingDocument.parentDocument);
+
+      if (parent?.isArchived) {
+        options.parentDocument = undefined;
+      }
+    }
+
+    const document = await ctx.db.patch(args.id, options);
+
+    recursiveRestore(args.id);
+
+    return document;
+  },
+});
+export const remove = mutation({
+  args: { id: v.id('documents') },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
+    const userId = identity.subject;
+
+    const exisingDocument = await ctx.db.get(args.id);
+
+    if (!exisingDocument) {
+      throw new Error('Document not found');
+    }
+    if (exisingDocument.userId !== userId) {
+      throw new Error('User unauthorized');
+    }
+
+    return await ctx.db.delete(args.id);
   },
 });
