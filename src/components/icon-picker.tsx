270cfe07587f2a492f4f83ae8bdd9bd393bb1297
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-07-16 22:39:56 +0600

    Toolbar component implemented. Document update by title and icon update.

---
+++ src/components/icon-picker.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import EmojiPicker, { Theme } from 'emoji-picker-react';
+import { useTheme } from 'next-themes';
+
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@/components/ui/popover';
+
+interface Props {
+  onChange: (icon: string) => void;
+  children: React.ReactNode;
+  asChild?: boolean;
+}
+
+export const IconPicker = ({ onChange, children, asChild }: Props) => {
+  const { resolvedTheme } = useTheme();
+  const currentTheme = (resolvedTheme || 'light') as keyof typeof themeMap;
+
+  const themeMap = {
+    dark: Theme.DARK,
+    light: Theme.LIGHT,
+  };
+
+  const theme = themeMap[currentTheme];
+  return (
+    <Popover>
+      <PopoverTrigger asChild={asChild}>{children}</PopoverTrigger>
+      <PopoverContent className="w-full border-none p-0 shadow-none">
+        <EmojiPicker
+          height={350}
+          theme={theme}
+          onEmojiClick={(data) => onChange(data.emoji)}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
