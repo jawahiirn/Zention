07f3936de64aa73d38a5901792e190d0e1795345
Nabhan <jawahiirboss@gmail.com>
2026-04-03 18:18:50 +0600

    updated public images updated global css

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from '@/shared/components/header';
 
 export default async function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center gap-12 p-4 md:p-4 font-sans bg-background/50 selection:bg-primary/20">
+    <div className="flex min-h-screen flex-col items-center gap-12 p-4 md:p-4 font-sansz selection:bg-primary">
       <Header />
       <main className="w-full max-w-5xl flex flex-col items-center gap-16 md:gap-24">
         <section className="text-center space-y-6 max-w-3xl">section</section>
