41414eb14d9532ddefb19fe664317ca265af3f9c
Nabhan <jawahiirboss@gmail.com>
2026-04-11 01:16:00 +0600

    update(proxy): - redirection to home page - onboarded condition check

--- proxy.ts
+++ proxy.ts
@@ -19,7 +19,7 @@ export function proxy(request: NextRequest) {
 
   // 2. If LOGGED IN -> Block Login/Signup pages
   if (isAuthPage) {
-    return NextResponse.redirect(new URL(hasOnboarded ? '/pokemon' : '/onboarding', request.url));
+    return NextResponse.redirect(new URL(hasOnboarded ? '/1234/home' : '/onboarding', request.url));
   }
 
   // 3. If NOT ONBOARDED -> Force Onboarding (unless already there)
@@ -27,10 +27,10 @@ export function proxy(request: NextRequest) {
     return NextResponse.redirect(new URL('/onboarding', request.url));
   }
 
-  // // 4. If ONBOARDED -> Block Onboarding page
-  // if (hasOnboarded && isOnboardingPage) {
-  //   return NextResponse.redirect(new URL('/pokemon', request.url));
-  // }
+  // 4. If ONBOARDED -> Block Onboarding page
+  if (hasOnboarded && isOnboardingPage) {
+    return NextResponse.redirect(new URL('/1234/home', request.url));
+  }
 
   return NextResponse.next();
 }
