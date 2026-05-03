8c090558fabe5047775a1e2dcc885369814ae52c
Nabhan <jawahiirboss@gmail.com>
2026-05-03 22:57:35 +0600

    remove: all individual api service files from each feature

--- features/pokemon/types/response.ts
+++
@@ -1,5 +0,0 @@
-import type { z } from 'zod';
-import type { pokemonDetailsSchema, pokemonListSchema } from '../schemas';
-
-export type PokemonListResponse = z.infer<typeof pokemonListSchema>;
-export type PokemonDetailsResponse = z.infer<typeof pokemonDetailsSchema>;
