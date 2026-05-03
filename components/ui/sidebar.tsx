3fbda96520791246729fa14a8bcbb0fa575d6499
Nabhan <jawahiirboss@gmail.com>
2026-05-03 23:06:02 +0600

    run script: npm run format (biome)

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -431,8 +431,6 @@ function SidebarMenu({ className, ...props }: React.ComponentProps<'ul'>) {
   );
 }
 
-
-
 function SidebarMenuItem({ className, ...props }: React.ComponentProps<'li'>) {
   return (
     <li
