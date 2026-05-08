93cff878ce1ebf5b9ede5944247ddbde4cf5e182
Nabhan <jawahiirboss@gmail.com>
2026-05-08 23:57:43 +0600

    test: pre-commit hook

--- shared/components/layout/workspace-shell.tsx
+++ shared/components/layout/workspace-shell.tsx
@@ -12,6 +12,7 @@ interface WorkspaceShellProps {
 }
 
 export function WorkspaceShell({ children, sidebarVariant, initialLayout }: WorkspaceShellProps) {
+  console.log('TEST: COMMIT HOOk');
   return (
     <>
       <SidebarHotkeys />
