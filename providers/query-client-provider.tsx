e605de11479fbd51b4df1d50f785aaf6b776015d
Nabhan <jawahiirboss@gmail.com>
2026-06-02 17:50:46 +0600

    install and integrate tanstack storage (persistor)

--- providers/query-client-provider.tsx
+++ providers/query-client-provider.tsx
@@ -1,10 +1,23 @@
-// In Next.js, this file would be called: app/providers.tsx
 'use client';
 
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { del, get, set } from 'idb-keyval';
 import type { ReactNode } from 'react';
 import { getQueryClient } from '@/services/query-client';
+
+const persister =
+  typeof window !== 'undefined'
+    ? createAsyncStoragePersister({
+        storage: {
+          getItem: (key: string) => get<string>(key),
+          setItem: (key: string, value: string) => set(key, value),
+          removeItem: (key: string) => del(key),
+        },
+      })
+    : undefined;
 
 interface Props {
   children: ReactNode;
@@ -12,10 +25,20 @@ interface Props {
 
 export const QueryProvider = ({ children }: Props) => {
   const queryClient = getQueryClient();
+
+  if (!persister) {
+    return (
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    );
+  }
+
   return (
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider client={queryClient} persistOptions={{ persister, maxAge: Infinity }}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   );
 };
