0a8b0cec3187616c9593e55c880e485ebf8127a4
Nabhan <jawahiirboss@gmail.com>
2026-06-03 06:00:16 +0600

    remove workspace switcher & create button ui from sidebar

--- shared/components/layout/app-sidebar.tsx
+++ shared/components/layout/app-sidebar.tsx
@@ -1,11 +1,9 @@
 import type { ComponentProps } from 'react';
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu } from '@/components/ui/sidebar';
-import { NAV_ITEMS, WORKSPACE_DATA } from '@/shared/constants/app-sidebar-constants';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarMenu } from '@/components/ui/sidebar';
+import { NAV_ITEMS } from '@/shared/constants/app-sidebar-constants';
 import { cn } from '@/shared/lib/utils';
 import { SidebarCollapseButton } from './sidebar-collapse-button';
-import { SidebarCreatePageButton } from './sidebar-create-page-button';
 import { SidebarNavLink } from './sidebar-nav-link';
-import { WorkspaceSwitcher } from './workspace-switcher';
 
 type AppSidebarProps = ComponentProps<typeof Sidebar> & {};
 // We will use the workspaceId & isCollapsed later...
@@ -21,13 +19,6 @@ export function AppSidebar({ variant = 'sidebar', className, ...props }: AppSide
       )}
       {...props}
     >
-      <SidebarHeader className="p-2 overflow-hidden">
-        <div className="flex items-center justify-between gap-1 w-full group-data-[collapsible=icon]:justify-center">
-          <WorkspaceSwitcher currentWorkspace={WORKSPACE_DATA} />
-          <SidebarCreatePageButton />
-        </div>
-      </SidebarHeader>
-
       <SidebarContent className="px-2">
         <SidebarMenu className="group-data-[collapsible=icon]:items-center">
           {NAV_ITEMS.map((item) => (
