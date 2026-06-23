d256a203becd7bfd7e90c927b79ad1d2652f5c61
Nabhan <jawahiirboss@gmail.com>
2026-06-24 00:06:06 +0600

    feat: add invitation modal feature

---
+++ features/invitation/components/invitation-container.tsx
@@ -0,0 +1,5 @@
+import { InvitationView } from './invitation-view';
+
+export const InvitationContainer = () => {
+  return <InvitationView />;
+};
---
+++ features/invitation/components/invitation-modal.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { Dialog, DialogContent } from '@/components/ui/dialog';
+import { InvitationContainer } from './invitation-container';
+
+interface InvitationModalProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export function InvitationModal({ open, onOpenChange }: InvitationModalProps) {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent showCloseButton={true} className="rounded-2xl flex flex-col p-6">
+        <InvitationContainer />
+      </DialogContent>
+    </Dialog>
+  );
+}
